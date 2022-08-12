ae00c0150ad5313b97425df33fa0e29b36b0be1e
Luke Hansen <lchansen@users.noreply.github.com>
2022-08-12 12:36:51 -0700

    Add project files.

---
+++ CodeContainerProviderSample/SampleCodeContainerProvider.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.CodeContainerManagement;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using CodeContainer = Microsoft.VisualStudio.Shell.CodeContainerManagement.CodeContainer;
+using ICodeContainerProvider = Microsoft.VisualStudio.Shell.CodeContainerManagement.ICodeContainerProvider;
+
+namespace CodeContainerProviderSample
+{
+    [Guid(GuidString)]
+    internal class SampleCodeContainerProvider : ICodeContainerProvider
+    {
+        public const string GuidString = "13562B13-0D64-4DEB-9464-6F6202511FA3";
+        public static Guid Guid = new Guid(GuidString);
+
+        public async Task<CodeContainer> AcquireCodeContainerAsync(IProgress<ServiceProgressData> downloadProgress, CancellationToken cancellationToken)
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+            var acquiredFolder = ShowFolderBrowserDialog(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Simulate Downloading a Code Container");
+
+            if (acquiredFolder != null)
+            {
+                string name = Path.GetFileName(acquiredFolder);
+                CodeContainerSourceControlProperties sccProperties = new CodeContainerSourceControlProperties(name, acquiredFolder, Guid);
+                CodeContainerLocalProperties localProperties = new CodeContainerLocalProperties(acquiredFolder, CodeContainerType.Folder, sccProperties);
+
+                // You might costruct this if you have a remote URL that this code container exists at
+                RemoteCodeContainer remoteContainer = null;
+
+                return new CodeContainer(localProperties, remoteContainer, isFavorite: false, lastAccessed: DateTimeOffset.UtcNow);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public Task<CodeContainer> AcquireCodeContainerAsync(Microsoft.VisualStudio.Shell.CodeContainerManagement.RemoteCodeContainer onlineCodeContainer, IProgress<ServiceProgressData> downloadProgress, CancellationToken cancellationToken)
+        {
+            // This would be called when VS can't find the local path (it may have been deleted),
+            // so this method might be used to re-download and get the new path for the code container.
+            throw new NotImplementedException();
+        }
+
+        private string ShowFolderBrowserDialog(string initialFolder, string dialogTitle = null)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            try
+            {
+                IVsUIShell uiShell = CodeContainerProviderSamplePackage.GetGlobalService(typeof(SVsUIShell)) as IVsUIShell;
+                if (uiShell == null)
+                {
+                    return null;
+                }
+
+                const int MaxBuffer = 2048;
+                VSBROWSEINFOW[] browseInfo = new VSBROWSEINFOW[1];
+
+                ErrorHandler.ThrowOnFailure(uiShell.GetDialogOwnerHwnd(out IntPtr ownerHwnd));
+
+                IntPtr dirNamePtr = Marshal.AllocCoTaskMem(MaxBuffer);
+                try
+                {
+                    browseInfo[0].pwzInitialDir = initialFolder;
+                    browseInfo[0].lStructSize = (uint)Marshal.SizeOf(typeof(VSBROWSEINFOW));
+                    browseInfo[0].pwzDlgTitle = dialogTitle;
+                    browseInfo[0].dwFlags = 0;
+                    browseInfo[0].nMaxDirName = 1024;
+                    browseInfo[0].pwzDirName = dirNamePtr;
+                    browseInfo[0].hwndOwner = ownerHwnd;
+
+                    return ErrorHandler.Succeeded(uiShell.GetDirectoryViaBrowseDlg(browseInfo))
+                        ? Marshal.PtrToStringAuto(browseInfo[0].pwzDirName)
+                        : null;
+                }
+                finally
+                {
+                    Marshal.FreeCoTaskMem(browseInfo[0].pwzDirName);
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
